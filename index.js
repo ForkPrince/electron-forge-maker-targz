5d21ca645f9a2b3ab37b33cf35f08bf526b8ba6b
Prince527 <Prince527gaming@gmail.com>
2025-06-29 18:57:25 -0300

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,25 @@
+const { MakerBase } = require("@electron-forge/maker-base");
+const path = require("path");
+const tar = require("tar");
+
+class MakerTarGz extends MakerBase {
+  name = "maker-targz";
+
+  async make({ dir, makeDir, targetPlatform, targetArch, packageJSON }) {
+    const output = path.join(makeDir, `${packageJSON.productName || packageJSON.name}-${targetPlatform}-${targetArch}-${packageJSON.version}.tar.gz`);
+
+    await tar.c(
+      {
+        file: output,
+        cwd: dir,
+        gzip: true,
+        portable: true,
+      },
+      ["."]
+    );
+
+    return [output];
+  }
+}
+
+module.exports = MakerTarGz;
